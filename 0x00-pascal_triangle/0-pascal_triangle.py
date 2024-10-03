a74978a1cb744daa525eb6cd9d38d80dc7ee4a5c
JuliusDgenius <ibejulius1@gmail.com>
2024-10-03 13:35:26 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,18 +13,6 @@ def pascal_triangle(n):
     """Returns pascal triangle"""
     if n <= 0:
         return []
-    triangle = []
-    for i in range(n):
-        temp_list = []
 
-        for j in range(i+1):
-            if j == 0 or j == i:
-                temp_list.append(1)
-            else:
-                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(temp_list)
-
-    return triangle
-
-    # return [[factorial(i) // (factorial(j)
-    #          * factorial(i - j)) for j in range(i + 1)] for i in range(n)]
+    return [[factorial(i) // (factorial(j)
+             * factorial(i - j)) for j in range(i + 1)] for i in range(n)]
