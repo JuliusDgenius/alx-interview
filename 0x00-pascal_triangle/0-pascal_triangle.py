458421e2abb4ef92f4ad72eb6069150c64289908
JuliusDgenius <ibejulius1@gmail.com>
2024-10-07 10:40:20 +0100

    remove import

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,17 +2,23 @@
 """
 that returns a list of lists of integers representing
 the Pascal’s triangle of n:
-
-- Returns an empty list if n <= 0
-- You can assume n will be always an integer
 """
-from math import factorial
 
 
 def pascal_triangle(n):
     """Returns pascal triangle"""
-    if n <= 0:
-        return []
+    triangle = []
 
-    return [[factorial(i) // (factorial(j)
-             * factorial(i - j)) for j in range(i + 1)] for i in range(n)]
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+
+    return triangle
