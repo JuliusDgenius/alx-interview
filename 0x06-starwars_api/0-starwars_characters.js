65f8bab3011a4b493d23707dea40149172685699
JuliusDgenius <ibejulius1@gmail.com>
2024-11-14 13:19:32 +0100

    add task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,3 +6,28 @@
   + You must use the request module
 **/
 
+const request = require('request');
+const filmId = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+request(url, async (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    const uris = data.characters;
+
+    for (const uri of uris) {
+      await new Promise((resolve, reject) => {
+        request(uri, (err, resp, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const charater = JSON.parse(body);
+            console.log(charater.name);
+            resolve();
+          }
+        });
+      });
+    }
+  }
+});
