0e7b24198c6457a18801561517467f5316dbf969
JuliusDgenius <ibejulius1@gmail.com>
2024-11-19 09:29:38 +0100

    update files

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,15 +12,23 @@ def rotate_2d_matrix(matrix):
     """
     Function to rotate a 2D matrix by 90 degrees clockwise.
     """
-    n = len(matrix)
+    # n = len(matrix)
 
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i + 1, n):  # Only swap elements above the diagonal
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # # Transpose the matrix
+    # for i in range(n):
+    #     for j in range(i + 1, n):  # Only swap elements above the diagonal
+    #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Reverse each row
-    for row in matrix:
-        row.reverse()
+    # # Reverse each row
+    # for row in matrix:
+    #     row.reverse()
 
-    return matrix
+    # return matrix
+    for i in range(int(len(matrix) / 2)):
+        for j in range(i, (len(matrix) - i - 1)):
+            x = (len(matrix) - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[(len(matrix) - i - 1)][x]
+            matrix[(len(matrix) - i - 1)][x] = matrix[j][(len(matrix) - i - 1)]
+            matrix[j][(len(matrix) - i - 1)] = tmp
